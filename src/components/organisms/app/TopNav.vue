1c326ec5bec0e8432fa28d9adedeaecf0acf0496
Igawa Hironobu <zin920314@gmail.com>
2020-05-26 08:58:29 +0900

    feat: 画面のフレームを作成

---
+++ src/components/organisms/app/TopNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="top-nav">
+    <div class="app-name">Techpit Chat</div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.top-nav {
+  display: flex;
+  height: 38px;
+  align-items: center;
+  justify-content: center;
+
+  background: #350d36;
+  color: #ffffff;
+  box-shadow: 0 1px 0 0 rgba(255, 255, 255, 0.1);
+
+  .app-name {
+    text-align: center;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/components/templates/AppTemplate.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-template">
+    <TopNav />
+
+    <div class="workspace">
+      <section class="workspace-sidebar"></section>
+
+      <section class="workspace-primary-view">
+        <slot />
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNav from '@/components/organisms/app/TopNav';
+
+export default {
+  components: {
+    TopNav
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app-template {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+
+  .top-nav {
+    flex: 0 0 auto;
+  }
+
+  .workspace {
+    flex: 1 1 0;
+  }
+}
+
+.workspace {
+  display: flex;
+
+  .workspace-sidebar {
+    flex: 0 0 260px;
+  }
+
+  .workspace-primary-view {
+    flex: 1 1 0;
+  }
+}
+
+.workspace-sidebar {
+  background-color: rgb(63, 14, 64);
+}
+</style>
