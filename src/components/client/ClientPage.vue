c2d1d5a1c78b9db32527ba75c756b65b3074471b
Igawa Hironobu <zin920314@gmail.com>
2021-01-30 15:59:33 +0900

    feat: クライアント フレームを実装

---
+++ src/components/client/ClientPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="client-page">
+    <ClientTopNav />
+
+    <div class="workspace">
+      <ClientSidebar />
+
+      <main class="primary-view">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClientTopNav from "@/components/client/topNav/ClientTopNav";
+import ClientSidebar from "@/components/client/sidebar/ClientSidebar";
+
+export default {
+  components: {
+    ClientTopNav,
+    ClientSidebar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.client-page {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+
+  .client-top-nav {
+    flex: 0 0 auto;
+  }
+
+  .workspace {
+    flex: 1 1 0;
+    min-height: 0;
+  }
+}
+
+.workspace {
+  display: flex;
+
+  .client-sidebar {
+    flex: 0 0 260px;
+  }
+
+  .primary-view {
+    flex: 1 1 0;
+    display: flex;
+  }
+}
+</style>
---
+++ src/views/client/ClientView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="client-view">
+    <ClientPage>
+      <router-view />
+    </ClientPage>
+  </div>
+</template>
+
+<script>
+import ClientPage from "@/components/client/ClientPage";
+
+export default {
+  components: {
+    ClientPage
+  }
+};
+</script>
